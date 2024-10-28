0d66f70fd266b9fe7f47504db8395d2064ec5891
Raul Darius Buda <126792326+darusln@users.noreply.github.com>
2024-10-28 16:59:23 +0200

    Update and rename porgram.py to porgram.cpp

---
+++ 1_Mini_Language/porgram.cpp
@@ -0,0 +1,25 @@
+int main(){
+    int x;
+    x = 10;
+    int y;
+    y = 5;
+    float z;
+    z = 0;
+    
+    cout<<"Enter a number:";
+    cin>>x;
+    
+    if (x > y){
+        z = x + y;
+    }
+    else{
+        z = x - y;
+    }
+    cout<<"The result is:";
+    cout<<z;
+    
+    while (z < 100){
+        z = z + 1;
+        cout<<z;
+    }
+}
